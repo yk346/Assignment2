b7a5893b2e98755ef2be1c6e32cdc87499221edd
yk346 <yevgeniykim1983@gmail.com>
2025-06-10 03:47:53 -0500

    calc mod

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,10 +1,20 @@
-from app.operations import addition, substraction, multiplication
+import pytest
+from app.operations import addition, subtraction, multiplication, division
 
 def test_addition():
     assert addition(1,1) == 2
 
-def test_substraction():
-    assert substraction(1,1) == 0
+def test_subtraction():
+    assert subtraction(1,1) == 0
 
 def test_multiplication():
     assert multiplication(1,1) == 1
+
+def test_division_positive():
+    assert division(1,1) == 1
+
+
+def test_division_by_zero():
+    """Test division by zero."""
+    with pytest.raises(ValueError, match="Division by zero is not allowed."):
+        division(1, 0)
